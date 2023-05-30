aebb00eba7c39ebc91575b7cce8da538b22a2ab4
박정호 <pakjeoungho@gmail.com>
2023-05-30 11:30:47 +0900

    hotfix

--- frontend/src/components/dish/ManageLog.tsx
+++ frontend/src/components/dish/ManageLog.tsx
@@ -24,7 +24,8 @@ export default function ManageLog() {
         </div>
       </div>
       <div className="w-full h-full bg-white">
-        <LogTable />
+        <div>hi</div>
+        {/* <LogTable /> */}
       </div>
     </div>
   );
